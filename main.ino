ae6247cf3742c70208285f9c547e53a303e6b17f
ShowMeMore <misterx_82@hotmail.com>
2017-07-31 04:07:37 +0200

    statemachine v1

--- main.ino
+++ main.ino
@@ -1,49 +1,63 @@
 #include <Wire.h>
-#include "chirp/chirp_driver.c"
-#include "led/led_driver.c"
-#include "statemachine/statemachine.c"
 
 /* globals */
 int state = 4;
 boolean plantState = true; // ok=true, nok=false
 byte n; // 8 bit unsigned integer
 boolean  mFlag, sFlag, lFlag = false;
-Ticker mTicker, sTicker, lTicker;
+Ticker measureTicker, sendTicker, ledTicker;
+
+#include "hardware/hardware.h"
+#include "led/led_driver.c"
+#include "chirp/chirp_driver.c"
+#include "statemachine/statemachine.c"
 
 void setup() {
   // initialize LED
-  led_setup();
+  led_setup(LED_PIN);
   // initialize chirp
   chirp_setup();
   // initialize serial monitor with baude rate
   Serial.begin(9600);
-  // creat two task, one 1s and the other 2s
-  mTicker.attach_us(mTicker_handle, 1000000);
-  sTicker.attach(sTicker_handle, 2);
+  // create two task, one 1s and the other 2s
+  //ticker for measure
+  measureTicker.attach(measureTicker_handle, 12);
+  //ticker to send --> implement later
+  //sendTicker.attach(sendTicker_handle, 2);
+  //ticker for led
+  ledTicker.attach(ledTicker_handle, 3);
 }
 
 void loop() {
   switch (state) {
 
-    case MEASURE: {
+    // measure
+    case 1: {
+      //LED indicates measurement
+      led_blink(LED_PIN,2000,0);
+      /* do measurement*/
       mFlag = false;
-      // measure…
+
+      //check plantState for LED
       if (!plantState) {
-        // Set Led Timer
+        /* do Set Led Timer */
       } else {
-        // Clear Led Timer
+        /* do Clear Led Timer */
       }
       /* determine transition & prep */
       if (sFlag) {
         state = CONNECT;
         n = 7;
+        Serial.println("Enter Connect Mode (m)");
       } else {
         state = SLEEP;
+        Serial.println("Enter Sleep Mode (m)");
       }
     }
     break;
 
-    case CONNECT: {
+    // connect --> implement later
+    case 2: {
       /* do */
       // setup ble adertising
       // wait?
@@ -51,6 +65,7 @@ void loop() {
       /* determine transition & prep */
       if (true) { //bleconnected
         state = SEND;
+        Serial.println("Enter Send Mode (c)");
       } else {
         if (!plantState) {
           if (n>0) {
@@ -58,16 +73,19 @@ void loop() {
           } else {
             sFlag = false;
             state = SLEEP;
+            Serial.println("Enter Sleep Mode (c)");
           }
         } else {
           sFlag = false;
           state = SLEEP;
+          Serial.println("Enter Sleep Mode (c)");
         }
       }
     }
     break;
 
-    case SEND: {
+    //send --> implement later
+    case 3: {
       /* do */
       // send data
       // clear data
@@ -76,13 +94,20 @@ void loop() {
       
       /* determine transition & prep */
       state = SLEEP;
+      Serial.println("Enter Sleep Mode (s)");
     }
     break;
 
-    case SLEEP: {
-      /* do */
-      if (mFlag) state = MEASURE;
-      // setup sleep mode
+    //sleep
+    case 4: {
+      Serial.println("sleeping...");
+      led_on(LED_PIN_BUILTIN);
+      if (mFlag) {
+        state = MEASURE;
+        led_off(LED_PIN_BUILTIN);
+        Serial.println("Enter Measure Mode (s)");
+      }
+      /* do  setup sleep mode*/
       
       /* determine transition & prep */
       //Switch state {
