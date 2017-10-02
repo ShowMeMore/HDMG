bba02f525a3b23bd0543a96dfdfc8d9bcf92fb0f
ShowMeMore <misterx_82@hotmail.com>
2017-10-02 07:24:52 +0200

    chnges for debug mode

--- files/statemachine.c
+++ files/statemachine.c
@@ -1,12 +1,17 @@
 void measureTicker_handle(void) {
+  debug_msg("statemachine: Set measureFlag to true... ", DEBUG_STATE);
   measureFlag = true; // wake up to measure
-  debug_msg_ln("statemachine: measureFlag set to true", DEBUG_STATE);
+  debug_msg_ln("DONE", DEBUG_STATE);
 }
+
 void sendTicker_handle(void) {
+  debug_msg("statemachine: Set sendFlag to true... ", DEBUG_STATE);
   sendFlag = true; // wake up to send
-  debug_msg_ln("statemachine: sendFlag set to true", DEBUG_STATE);
+  debug_msg_ln("DONE", DEBUG_STATE);
 }
+
 void ledTicker_handle(void) {
+  debug_msg("statemachine: Set ledFlag to true... ", DEBUG_STATE);
   ledFlag = true; // wake up to blink LED
-  debug_msg_ln("statemachine: ledFlag set to true", DEBUG_STATE);
+  debug_msg_ln("DONE", DEBUG_STATE);
 }
