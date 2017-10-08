7570b558eaaa73f089ade212370d58fb86cfa9ef
ShowMeMore <misterx_82@hotmail.com>
2017-10-08 21:04:22 +0200

    New: ledStopTicker_handle() Rename: ledTicker_handle() --> ledBlinkTicker_handle()

--- files/statemachine.c
+++ files/statemachine.c
@@ -4,14 +4,21 @@ void measureTicker_handle(void) {
   debug_msg_ln("DONE", DEBUG_STATE);
 }
 
-void sendTicker_handle(void) {
-  debug_msg("statemachine: Set sendFlag to true... ", DEBUG_STATE);
-  sendFlag = true; // wake up to send
+void ledBlinkTicker_handle(void) {
+  debug_msg("statemachine: Set ledFlag to true... ", DEBUG_STATE);
+  ledFlag = true; // wake up to blink LED
   debug_msg_ln("DONE", DEBUG_STATE);
 }
 
-void ledTicker_handle(void) {
-  debug_msg("statemachine: Set ledFlag to true... ", DEBUG_STATE);
-  ledFlag = true; // wake up to blink LED
+void ledStopTicker_handle(void) {
+  debug_msg("statemachine: Set ledFlag to false... ", DEBUG_STATE);
+  ledFlag = false; // stop blink LED
+  ledBlinkTicker.detach();
+  debug_msg_ln("DONE", DEBUG_STATE);
+}
+
+void sendTicker_handle(void) {
+  debug_msg("statemachine: Set sendFlag to true... ", DEBUG_STATE);
+  sendFlag = true; // wake up to send
   debug_msg_ln("DONE", DEBUG_STATE);
 }
