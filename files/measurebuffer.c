48ed08b82ebb62156c1723bb5a4367f94af89915
ShowMeMore <misterx_82@hotmail.com>
2017-10-08 21:08:41 +0200

    Change: Buffer[12] --> Buffer[24] Change: writeMeasureBuffer() --> if buffer full, replace latest value Change: some changes to readMeasureBuffer()

--- files/measurebuffer.c
+++ files/measurebuffer.c
@@ -1,34 +1,48 @@
-void clearMeasureBuffer(void){
-  debug_msg("measurebuffer: Clear MeasureBuffer... ", DEBUG_STATE);
-  for (int i = 0; i < 12; ++i) {
-    MeasureBuffer[i] = 0;
-  }
-  debug_msg_ln("DONE", DEBUG_STATE);
-}
-
 void writeMeasureBuffer(int value){
-  debug_msg("measurebuffer: Write to MeasureBuffer... ", DEBUG_STATE);
+  debug_msg("measurebuffer: Write to MeasureBuffer... ", DEBUG_VALUES);
   boolean writeStatus = false;
-  for (int i = 0; i < 12; ++i) {
-    if (!writeStatus && (MeasureBuffer[i] == 0)){
-      MeasureBuffer[i] = value;
-      writeStatus = true;
-    }
+  for (int i = 0; i < 24; ++i) {
+    if (!writeStatus && (MeasureBuffer[23] == -1)) {
+      if (MeasureBuffer[i] == -1) {
+        MeasureBuffer[i] = value;
+        writeStatus = true;
+      }
+    } else if (!writeStatus && (MeasureBuffer[23] != -1)) {
+        if (i > 0) {
+          MeasureBuffer[i-1] = MeasureBuffer[i];
+          if (i == 23) {
+            MeasureBuffer[i] = value;
+            writeStatus = true;
+          }
+        }
+      }
   }
   if (writeStatus){
-    debug_msg_ln("DONE", DEBUG_STATE);
+    debug_msg_ln("DONE", DEBUG_VALUES);
   }else{
-    debug_msg_ln("ERROR", DEBUG_STATE);
+    debug_msg_ln("ERROR", DEBUG_VALUES);
   }
 }
 
 void readMeasureBuffer(void){
-  debug_msg_ln("measurebuffer: Read MeasureBuffer... ", DEBUG_STATE);
-  for (int i = 0; i < 12; ++i) {
-    debug_msg(MeasureBuffer[i], DEBUG_STATE);
-    debug_msg(" / ", DEBUG_STATE);
+  debug_msg_ln("measurebuffer: Read MeasureBuffer... ", DEBUG_VALUES);
+  for (int i = 0; i < 24; ++i) {
+    if (MeasureBuffer[i] != -1) {
+      debug_msg(i, DEBUG_VALUES);
+      debug_msg(": ", DEBUG_VALUES);
+      debug_msg(MeasureBuffer[i], DEBUG_VALUES);
+      debug_msg(" / ", DEBUG_VALUES);
+    }
   }
-  //debug_msg_ln("DONE", DEBUG_STATE);
-  debug_msg_ln("", DEBUG_STATE);
-  debug_msg_ln("measurebuffer: Read MeasureBuffer... DONE", DEBUG_STATE);
+  //debug_msg_ln("DONE", DEBUG_VALUES);
+  debug_msg_ln("", DEBUG_VALUES);
+  debug_msg_ln("measurebuffer: Read MeasureBuffer... DONE", DEBUG_VALUES);
+}
+
+void clearMeasureBuffer(void){
+  debug_msg("measurebuffer: Clear MeasureBuffer... ", DEBUG_VALUES);
+  for (int i = 0; i < 24; ++i) {
+    MeasureBuffer[i] = -1;
+  }
+  debug_msg_ln("DONE", DEBUG_VALUES);
 }
